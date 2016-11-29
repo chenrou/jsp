6b4378afc354aa67de1e65682690448d9ca83921
Your Name <you@examp                                                            le.com>
2016-11-29 20:18:56 +0800

    实训10.3页面

---
+++ ceshi/src/message/AddMessageServlet.java
@@ -0,0 +1,53 @@
+package message;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/AddMessageServlet2")
+public class AddMessageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public AddMessageServlet() {
+        super();
+    }
+
+    protected void doGet (HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
+		doPost(request,response);
+	}
+
+    protected void doPost (HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
+		String author = new String (request.getParameter("author").getBytes("ISO-8859-1"),"utf-8");
+		String title = new String (request.getParameter("title").getBytes("ISO-8859-1"),"utf-8");
+		String content = new String (request.getParameter("Content").getBytes("ISO-8859-1"),"utf-8");
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String today = format.format(new Date());
+		
+		MessageBean mm = new MessageBean();
+		mm.setAuthor(author);
+		mm.setTitle(title);
+		mm.setContent(content);
+		mm.setTime(today);
+		
+		ArrayList wordlist = (ArrayList)request.getAttribute("wordlist");
+		if(wordlist==null){
+			wordlist=new ArrayList();
+		}
+		wordlist.add(mm);
+		request.setAttribute("wordlist", wordlist);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("showMessage.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
